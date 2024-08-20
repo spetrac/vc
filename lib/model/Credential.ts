f6ca6d7ceb2d14b02c593695cedd5884951b87d2
Simon Petrac <spetrac@nicos-ag.com>
2024-08-20 15:53:23 +0200

    added v1/v2 date

--- lib/model/Credential.ts
+++ lib/model/Credential.ts
@@ -1,10 +1,10 @@
 import { assertRecord, assertArray, toArray, isRecord } from './Util.js'
-import { type AnyURI, assertAnyURI } from './XSD.js'
-import { type CredentialContext, assertCredentialContext } from './CredentialContext.js'
-import { assertType, Type } from './JSON-LD.js'
+import { type AnyURI, assertAnyURI, type DateTime, assertDateTime } from './XSD.js'
+import { type CredentialContext, type CredentialContextVersion, assertCredentialContext, extractCredentialContextVersion } from './CredentialContext.js'
+import { assertType, type Type } from './JSON-LD.js'
 import { assertCredentialSubject, type CredentialSubject } from './CredentialSubject.js'
 import { assertCredentialIssuer, type CredentialIssuer } from './CredentialIssuer.js'
-import { assertCredentialStatus, CredentialStatus } from './CredentialStatus.js'
+import { assertCredentialStatus, type CredentialStatus } from './CredentialStatus.js'
 
 export * from './CredentialContext.js'
 export * from './CredentialIssuer.js'
@@ -30,13 +30,17 @@ export type Credential<version = 'any'> = version extends 'base' ? {
   // TODO evidence
   // TODO mustHaveType
 } : version extends 1.0 ? Credential<'base'> & {
-  // TODO v1 specifics
+  issuanceDate: DateTime,
+  expirationDate?: DateTime
 } : version extends 2.0 ? Credential<'base'> & {
-  // TODO v2 specifics
+  validFrom?: DateTime,
+  validUntil?: DateTime
 } : Credential<1.0> | Credential<2.0>
 
-export function assertCredential(credential: unknown, options?: { path?: string }): asserts credential is Credential {
+export function assertCredential(credential: unknown, options?: { path?: string, mode?: 'issue' | 'verify', now?: Date | DateTime }): asserts credential is Credential {
   const _path = options?.path ?? 'credential'
+  const _mode = options?.mode ?? 'verify'
+  const _now = new Date(options?.now ?? Date.now())
   assertRecord(credential, { path: _path })
   assertCredentialContext(credential['@context'], { path: `${_path}.@context` })
   if ('id' in credential) assertAnyURI(credential.id, { path: `${_path}.id` })
@@ -44,9 +48,28 @@ export function assertCredential(credential: unknown, options?: { path?: string 
   assertCredentialIssuer(credential.issuer, { path: `${_path}.issuer`, allowRecord: true, allowArray: true })
   assertCredentialSubject(credential.credentialSubject, { path: `${_path}.credentialSubject`, allowMultiple: true })
   if ('credentialStatus' in credential) assertCredentialStatus(credential.credentialStatus, { path: `${_path}.credentialStatus`, allowMultiple: true })
-  // TODO v1 specifics
-  // TODO v2 specifics
+  switch (extractCredentialContextVersion(credential['@context'])) {
+    case 1:
+      assertCredentialValidDate(credential.issuanceDate, { path: `${_path}.issuanceDate`, max: _mode === 'verify' ? _now : undefined })
+      if ('expirationDate' in credential) assertCredentialValidDate(credential.expirationDate, { path: `${_path}.expirationDate`, min: _mode === 'verify' ? _now : undefined })
+      break;
+    case 2:
+      if ('validFrom' in credential) assertCredentialValidDate(credential.validFrom, { path: `${_path}.validFrom`, max: _mode === 'verify' ? _now : undefined })
+      if ('validUntil' in credential) assertCredentialValidDate(credential.validUntil, { path: `${_path}.validUntil`, min: _mode === 'verify' ? _now : undefined })
+      break;
+  }
   // TODO evidence
   // TODO mustHaveType
 }
 
+export function assertCredentialValidDate(value: unknown, options?: { path?: string, min?: Date, max?: Date }): asserts value is DateTime {
+  const _path = options?.path ?? 'value'
+  assertDateTime(value, { path: _path })
+  if (options?.min || options?.max) {
+    const date = new Date(value)
+    if (options?.min && date < options.min)
+      throw new Error(`${_path} must be not be before ${options.min.toISOString()}`)
+    if (options?.max && date > options.max)
+      throw new Error(`${_path} must be not be after ${options.max.toISOString()}`)
+  }
+}
